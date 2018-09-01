d8d0c87cd934c04218b990ffe64e924fe28beb36
Ju Hyuk Park <jpark@roo.kr>
2018-09-02 00:03:42 +0900

    모델 변경

--- bankco2/models.py
+++ bankco2/models.py
@@ -2,8 +2,26 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Animal(models.Model):
+    code = models.CharField(max_length=16)
+    image = models.FileField(null=True, blank=True)
+    score = models.FloatField(default=1)
+
+
+class Device(models.Model):
+    device_id = models.CharField(max_length=100, blank=False, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+
+class CustomUser(User):
+    device = models.ForeignKey(Device, null=True, blank=True, on_delete=models.SET_NULL)
+    animal = models.ManyToManyField(Animal)
+
+
 class Step(models.Model):
-    device_id = models.CharField(max_length=255, default='default-id')
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
     count = models.IntegerField()
     step_date = models.DateField()
 
