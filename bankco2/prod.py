0db89be8fc710fef745b316b3f44ea98d68ead34
Taehwan Kim <maxtortime@gmail.com>
2018-09-01 20:54:16 +0900

    Add mysql setting

---
+++ bankco2/prod.py
@@ -0,0 +1,9 @@
+from .settings import *
+
+DEBUG = False
+DATABASES = {
+    'NAME': 'bankco2',
+    'HOST': 'localhost',
+    'USER': 'bankco2',
+    'PASSWORD': 'bankco2'
+}
--- bankco2/settings.py
+++ bankco2/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -130,8 +130,3 @@ ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS = True
 LOGIN_REDIRECT_URL = "/api"
 ACCOUNT_AUTHENTICATED_LOGOUT_REDIRECTS = True
 ACCOUNT_LOGOUT_REDIRECT_URL = "/api"
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
