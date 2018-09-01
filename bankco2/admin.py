e3724986e2936b9cdfb15d301bee852e8bc1d173
Ju Hyuk Park <jpark@roo.kr>
2018-09-02 01:57:51 +0900

    animal admin view 추가

--- bankco2/admin.py
+++ bankco2/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from bankco2.models import Step, Device
+from bankco2.models import Step, Device, Animal
 
-admin.site.register(Step)
-admin.site.register(Device)
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    pass
+
