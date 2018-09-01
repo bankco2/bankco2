1573ecd667c5b29a8607ca9e084134a41b5cf75b
root <root@ip-10-0-0-177.ap-northeast-2.compute.internal>
2018-09-01 16:25:05 +0000

    prod setting

---
+++ bankco2/prod.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'NAME': 'bankco2',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'bankco2'
+    }
+}
